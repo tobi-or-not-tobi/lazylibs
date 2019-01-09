d51bd94982ba6da46bc0ebcb5661654a815ba061
Tobias Ouwejan <tobias.ouwejan@sap.com>
2019-01-09 12:24:55 +0100

    Simplify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RoutingModule } from 'componentlib';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule, RoutingModule.forRoot()],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'comp-1',
+        loadChildren: 'componentlib/comp-1#Comp1Module'
+      },
+      {
+        path: '**',
+        redirectTo: 'comp-1'
+      }
+    ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
